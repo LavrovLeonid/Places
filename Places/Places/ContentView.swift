a33fca6ff64ad84fae28f6a615c0020133135b09
LavrovLeonid <lavrovleonid94@gmail.com>
2021-01-31 10:53:44 +0500

    Edit single place item

--- Places/Places/ContentView.swift
+++ Places/Places/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("")
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }.padding()
     }
 }
 
