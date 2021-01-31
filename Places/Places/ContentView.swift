d3165150c442cfc3c17f3b72359073343e4b6ff3
LavrovLeonid <lavrovleonid94@gmail.com>
2021-01-31 10:10:38 +0500

    Create project

---
+++ Places/Places/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Places
+//
+//  Created by Leonid Lavrov on 31.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
