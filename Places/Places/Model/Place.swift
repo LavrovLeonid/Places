b5aad0d472c959a8237ba4a5c02a5d747ad87307
LavrovLeonid <lavrovleonid94@gmail.com>
2021-01-31 22:00:15 +0500

    Model

---
+++ Places/Places/Model/Place.swift
@@ -0,0 +1,37 @@
+//
+//  Place.swift
+//  Places
+//
+//  Created by Leonid Lavrov on 31.01.2021.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct Place: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+}
+
+
